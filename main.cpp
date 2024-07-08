ebdb6afec79c5ffd5d138a3ef9af8e4b62a38e86
Skooshie <james.mcenaney00@gmail.com>
2024-07-08 17:54:55 +0000

    ADDSUB

--- main.cpp
+++ main.cpp
@@ -2,12 +2,18 @@
 #include "mbed.h"
 
 int main()
+    // Set up integers a,b,c,d,e 
 {
-    int a = 10, b = 20, c = 30, d,e;
-    
+    int a = 10, b = 20, c = 30, d, e;
+
+    //Do the Addition and the subtraction.
+
     d = a + b;
     e = c - d;
-    printf("Numbers a + b = d = %d\n",d);
-    printf("Numbers c - d = e = %d",e);
+
+    // Send the results to the screen
+
+    printf("Addition a + b = d = %d\n",d);
+    printf("Subtraction c - d = e = %d\n",e);
     return 0;
 }
